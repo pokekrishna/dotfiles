1032a2ed0eb6b9c345a82ad8ffb8c2878d3fe53c
krishna.gupta <krishna.gupta@gojek.com>
2022-07-10 11:56:27 +0530

    Update readme

--- qemu/from-iso.sh
+++ qemu/from-iso.sh
@@ -3,6 +3,8 @@
 efi_firm="$(dirname $(which qemu-img))/../share/qemu/edk2-aarch64-code.fd"
 #dd if=/dev/zero conv=sync bs=1m count=64 of=ubuntu_ovmf_vars.fd #Only during OS install
  
+host_dir="/Users/krishnagupta/Documents"
+
 qemu-system-aarch64 \
   -serial stdio \
   -M virt,highmem=off \
@@ -21,5 +23,5 @@ qemu-system-aarch64 \
   -drive file=ubuntu_ovmf_vars.fd,if=pflash,index=1,format=raw \
   -drive file=ubuntu.qcow2,if=virtio,cache=writethrough \
   -netdev user,id=n0,hostfwd=tcp::2222-:22 -device virtio-net-pci,netdev=n0 \
-  -virtfs local,path=/Users/krishnagupta/Documents,mount_tag=shost0,security_model=passthrough,id=host0 \
+  -virtfs local,path=${host_dir},mount_tag=host0,security_model=passthrough,id=host0 \
 #  -cdrom ../focal-desktop-arm64.iso #Only during OS install
